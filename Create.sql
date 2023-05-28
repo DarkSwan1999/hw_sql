67d601ca349e08086ad5f32125303e9308fd0e2d
DarkSwan1999 <124700857+DarkSwan1999@users.noreply.github.com>
2023-05-28 13:13:35 +0300

    Add files via upload

--- Create.sql
+++ Create.sql
@@ -1,47 +1,46 @@
---CREATE TABLE genres (
---id SERIAL PRIMARY KEY,
---name TEXT NOT NULL
---);
+CREATE TABLE genres (
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL
+);
 
---CREATE TABLE artists (
---id SERIAL PRIMARY KEY,
---name TEXT NOT NULL
---);
+CREATE TABLE artists (
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL
+);
 
---CREATE TABLE albums (
---id SERIAL PRIMARY KEY,
---name TEXT NOT NULL,
---year INTEGER NOT NULL,
---artist_id INTEGER not null references artists(id)
---);
---
---CREATE TABLE tracks (
---id SERIAL PRIMARY KEY,
---name TEXT NOT NULL,
---duration INTERVAL NOT NULL,
---album_id INTEGER NOT NULL REFERENCES albums(id)
---);
---
---CREATE TABLE artist_genre (
---artist_id INTEGER NOT NULL REFERENCES artists(id),
---genre_id INTEGER NOT NULL REFERENCES genres(id),
---PRIMARY KEY (artist_id, genre_id)
---);
---
---CREATE TABLE album_artist (
---album_id INTEGER NOT NULL REFERENCES albums(id),
---artist_id INTEGER NOT NULL REFERENCES artists(id),
---PRIMARY KEY (album_id, artist_id)
---);
---
---CREATE TABLE compilations (
---id SERIAL PRIMARY KEY,
---name TEXT NOT NULL,
---year INTEGER NOT NULL
---);
---
---CREATE TABLE compilation_track (
---compilation_id INTEGER NOT NULL REFERENCES compilations(id),
---track_id INTEGER NOT NULL REFERENCES tracks(id),
---PRIMARY KEY (compilation_id, track_id)
---);
+CREATE TABLE albums (
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL,
+year INTEGER NOT NULL
+);
+
+CREATE TABLE tracks (
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL,
+duration INTERVAL NOT NULL,
+album_id INTEGER NOT NULL REFERENCES albums(id)
+);
+
+CREATE TABLE artist_genre (
+artist_id INTEGER NOT NULL REFERENCES artists(id),
+genre_id INTEGER NOT NULL REFERENCES genres(id),
+PRIMARY KEY (artist_id, genre_id)
+);
+
+CREATE TABLE album_artist (
+album_id INTEGER NOT NULL REFERENCES albums(id),
+artist_id INTEGER NOT NULL REFERENCES artists(id),
+PRIMARY KEY (album_id, artist_id)
+);
+
+CREATE TABLE compilations (
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL,
+year INTEGER NOT NULL
+);
+
+CREATE TABLE compilation_track (
+compilation_id INTEGER NOT NULL REFERENCES compilations(id),
+track_id INTEGER NOT NULL REFERENCES tracks(id),
+PRIMARY KEY (compilation_id, track_id)
+);
--- newSelects.sql
+++ newSelects.sql
@@ -14,37 +14,42 @@ FROM albums a
 LEFT JOIN tracks t ON a.id = t.album_id
 GROUP BY a.name;
 
-SELECT DISTINCT a.name as artist_name
+SELECT a.name AS artist_name
 FROM artists a
-LEFT JOIN albums al ON a.id = al.artist_id
-WHERE al.year IS NULL OR al.year != 2020;
+WHERE a.name NOT IN (
+  SELECT a.name
+  FROM artists a
+  JOIN album_artist aa ON a.id = aa.artist_id
+  JOIN albums al ON aa.album_id = al.id
+  WHERE al.year = 2020
+);
 
-
-SELECT c.name as collection_name
-FROM collections c
-LEFT JOIN collection_track ct ON c.id = ct.collection_id
+SELECT c.name as compilations_name
+FROM compilations c
+LEFT join compilation_track ct ON c.id = ct.compilation_id 
 LEFT JOIN tracks t ON ct.track_id = t.id
 LEFT JOIN albums a ON t.album_id = a.id
 LEFT JOIN artists ar ON a.artist_id = ar.id
 WHERE ar.name = 'Michael Djacson';
 
 
-SELECT a.name as album_name
+SELECT DISTINCT a.name AS album_name
 FROM albums a
-LEFT JOIN artists ar ON a.artist_id = ar.id
-LEFT JOIN artist_genre ag ON ar.id = ag.artist_id
-GROUP BY a.name
+JOIN album_artist aa ON a.id = aa.album_id
+JOIN artists ar ON aa.artist_id = ar.id
+JOIN artist_genre ag ON ar.id = ag.artist_id
+GROUP BY a.name, ag.artist_id
 HAVING COUNT(DISTINCT ag.genre_id) > 1;
 
-SELECT t.name as track_name
+SELECT t.name AS track_name
 FROM tracks t
-LEFT JOIN collection_track ct ON t.id = ct.track_id
-WHERE ct.collection_id IS NULL;
+LEFT JOIN compilation_track ct ON t.id = ct.track_id
+WHERE ct.compilation_id IS NULL;
 
 SELECT a.name as artist_name, t.name as track_name, t.duration as track_duration
 FROM tracks t
 LEFT JOIN albums al ON t.album_id = al.id
-LEFT JOIN artists a ON al.artist_id = a.id
+LEFT JOIN artists a ON a."name"  = a.id
 WHERE t.duration = (SELECT MIN(duration) FROM tracks);
 
 SELECT a.name as album_name, COUNT(t.id) as number_of_tracks
